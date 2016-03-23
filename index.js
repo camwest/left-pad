a26612a456c3f137731051ef987a0a0677c4f72d
Walker Hamilton <hey@walkerhamilton.com>
2016-03-22 20:14:40 -0500

    ternary

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = leftpad;
 
 function leftpad (str, len, ch) {
-  if (typeof(ch)=='undefined') ch = ' ';
+  ch = (typeof(ch)=='undefined') ? ' ' : ch;
 
   while (String(str).length < len-1) str = ch + str;
 
