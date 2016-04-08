df6ef71a450b6e6747e2716b56db00197d04c901
Nate Watson <natewatson999@gmail.com>
2016-04-08 12:01:09 -0400

    Added support for multi-character pad statements.
    In the previous version, this worked:
    :var leftpad = require("left-pad");
    :console.log(leftpad("abcd", 26, "P"));
    
    But this did not:
    :var leftpad = require("left-pad");
    :console.log(leftpad("abcd", 26, "PP"));
    
    An incorrect number of pad characters would be added.

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ module.exports = leftpad;
 function leftpad (str, len, ch) {
   str = String(str);
 
-  var i = -1;
+  if (ch == null) {
+    ch = ' ';
+  }
 
-  if (!ch && ch !== 0) ch = ' ';
-
-  len = len - str.length;
-
-  while (++i < len) {
+  while (str.length < len) {
     str = ch + str;
   }
 
