a1f52d4a11a40aaf6bd42154947b277269087785
Nate Watson <natewatson999@gmail.com>
2016-04-09 18:05:22 -0400

    Changed from O(N^2) to O(N*LOG(2,N))
    In most Javascript interpreters, in concatenation, in the creation of the result string, each character of each string must be iterated through. In the old version, there was a loop of each required iteration. As a result, with N concatenations and N characters, the performance is O(N^2). In this version, the number of concatenations is logarithmic to the number of strings to be combined, with a base of somewhere between 2 and 3. As a result, this is O(N*LOG(K,N)). Though it requires a few kilobytes more memory, this is a major performance improvement at large scales.

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-module.exports = leftpad;
-
-function leftpad (str, len, ch) {
+function lp (str, len, ch) {
+  var treePad = function(phrase, count){
+    switch(count){
+      case 0: return "";
+      case 1: return phrase;
+      default:
+        if ((count%2)==0) {
+          var inter = treePad(phrase, count/2);
+          return (inter + inter);
+        }
+        if ((count%3)==0) {
+            var interm = treePad(phrase, count/3);
+            return (interm + interm + interm);
+        }
+        return (phrase + treePad(phrase, count-1));
+    }
+  };
+  if (ch==null) {
+    return lp(str,len,' ');
+  }
   str = String(str);
-
-  if (ch == null) {
-    ch = ' ';
+  var padLen = len-str.length;
+  var padCount = Math.floor(padLen/ch.length);
+  if((ch.length % padLen)==0) {
+    return (treePad(ch, padCount)+str);
   }
-
-  while (str.length < len) {
-    str = ch + str;
-  }
-
-  return str;
-}
+  return (treePad(ch, padCount+1)+str);
+};
+module.exports = lp;
